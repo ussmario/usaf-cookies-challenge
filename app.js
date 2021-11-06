00fe5390c5f8e098f042713ae5d2065d20df81a0
Mario <matthew.davies.3@spaceforce.mil>
2021-11-06 19:30:17 -0400

    add comments andd cookie options

--- app.js
+++ app.js
@@ -3,13 +3,18 @@ const cookieParser = require('cookie-parser');
 const app = express();
 const port = 3000;
 
-// app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(cookieParser());
 
 app.post('/login', (req, res) => {
+  //postman post request {"name": "Mario"} in body as json
   let userName = req.body;
-  userName.name && res.cookie('name', userName.name);
+  //if necessary options can be defined here and included in res.cookie
+  let options = {
+    maxAge:9000000,
+    httpOnly: true
+  }
+  userName.name && res.cookie('name', userName.name, options);
   res.redirect('/hello');
 })
 
